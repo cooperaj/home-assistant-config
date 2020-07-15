b337d961137f40d00f75223d398854650391d76c
Adam Cooper <adam@networkpie.co.uk>
2020-07-15 13:22:02 +0000

    Don't need this anymore

--- create-volumes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/traefik-config --name=traefik-config
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/mosquitto-data --name=mosquitto-data
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/mosquitto-config --name=mosquitto-config
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/homeassistant-config --name=homeassistant-config
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/nodered-data --name=nodered-data
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/pihole-config --name=pihole-config
-docker volume create -d local-persist -o mountpoint=$(pwd)/data/dnsmasq-config --name=dnsmasq-config
